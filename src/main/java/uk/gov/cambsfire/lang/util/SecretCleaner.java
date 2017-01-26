fa998d317a6f0df65a5250364c412a9a01bdc214
Dave Bower <dave@lourish.com>
2017-01-26 15:47:57 +0000

    Added secret cleaner

--- src/main/java/uk/gov/cambsfire/lang/util/SecretCleaner.java
+++ src/main/java/uk/gov/cambsfire/lang/util/SecretCleaner.java
@@ -8,18 +8,18 @@ package uk.gov.cambsfire.lang.util;
  * %%
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. Neither the name of the Cambridgeshire Fire and Rescue Service nor the names of its contributors
  *    may be used to endorse or promote products derived from this software without
  *    specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -33,15 +33,15 @@ package uk.gov.cambsfire.lang.util;
  * #L%
  */
 
-
 import java.util.regex.Pattern;
 
 public final class SecretCleaner {
 
     private static final String SECRET_REGEX =
-            "([\\\"']?\\w*?(password|secret)\\w*?[\\\"']?)(\\s*\\:?\\s*)?['\\\"]?.+?['\\\"]?(([,}\\]])|$)";
+            "([\\\"']?\\w*?(\\w*password\\w*|\\w*secret\\w*)\\w*?[\\\"']?)"
+                    + "(\\s*\\:?\\s*)?['\\\"]?.+?['\\\"]?(([,}\\]])|$)";
     private static final String SECRET_REPLACEMENT_STRING = "$1$3'*****'$4";
-    private static final Pattern SECRET_PATTERN = Pattern.compile(SECRET_REGEX);
+    private static final Pattern SECRET_PATTERN = Pattern.compile(SECRET_REGEX, Pattern.CASE_INSENSITIVE);
 
     private SecretCleaner() {
         // utils
--- src/test/java/uk/gov/cambsfire/lang/util/SecretCleanerTest.java
+++ src/test/java/uk/gov/cambsfire/lang/util/SecretCleanerTest.java
@@ -8,18 +8,18 @@ package uk.gov.cambsfire.lang.util;
  * %%
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. Neither the name of the Cambridgeshire Fire and Rescue Service nor the names of its contributors
  *    may be used to endorse or promote products derived from this software without
  *    specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -32,7 +32,6 @@ package uk.gov.cambsfire.lang.util;
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  * #L%
  */
-
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -53,4 +52,13 @@ public class SecretCleanerTest {
                 .isEqualTo("[abc: \"123\", password: '*****']");
 
     }
+
+    @Test
+    public void removeSecretsRemovesDataValuesLinkedToOtherPasswords() {
+        assertThat(SecretCleaner.removeSecrets("'abc': 123, 'confirmPassword': 'bob'"))
+                .isEqualTo("'abc': 123, 'confirmPassword': '*****'");
+        assertThat(SecretCleaner.removeSecrets("'abc': 123, 'passwordTemp': 'bob'"))
+                .isEqualTo("'abc': 123, 'passwordTemp': '*****'");
+    }
+
 }
